3d730d51fcddd98e36fc23f83798f9b352b3d0c8
David BOKIL <bokilmusic@Davids-Air.fios-router.home>
2017-03-22 15:35:25 -0400

    progress

--- assets/javascript/logic copy.js
+++ assets/javascript/logic copy.js
@@ -1,34 +1,68 @@
-var shows = ["Game of Thrones", "Breaking Bad", "Mad Men", "The Sopranos", "The Wire"]
+ var shows = ["Game of Thrones", "Breaking Bad", "Mad Men", "The Sopranos", "The Wire"]
 
-// Dynamically Create buttons for the initial titles in the array
+
 function renderButtons() {
-    $("#tvButtons").empty();
+    $("#gifs-appear-here").empty();
     for (var i = 0; i < shows.length; i++) {
         var a = $("<button>");
-        a.addClass("btn btn-default");
-        a.attr("data-name", shows[i]);
+        a.addClass("button btn btn-default gifButton");
+        a.attr("data-person", shows[i]);
         a.text(shows[i]);
-        $("#tvButtons").append(a);
+        $("#gifs-appear-here").append(a);
+console.log(a)
     }
 }
+        console.log(shows)
 
-$("#addButton").on("click", function(event){
-    event.preventDefault();
-	var showrequest = $("#tvshowInput").val().trim();
-	shows.push(showrequest);
-	renderButtons()
-	console.log(showrequest)
-
-});
+        console.log(shows.length)
 
 renderButtons()
 
-// AJAX Call
+// $("#addButton").on("click", function(event){
+//     event.preventDefault();
+//     var showrequest = $("#tvshowInput").val().trim();
+//     shows.push(showrequest);
+//     // renderButtons()
+// });
 
-var movie = $(this).attr("data-name");
-console.log(movie)
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
-$.ajax({
+    // Event listener for all button elements
+    $(".gifButton").on("click", function() {
+      // In this case, the "this" keyword refers to the button that was clicked
+      var person = $(this).attr("data-person");
+      console.log(person)
+
+      // Constructing a URL to search Giphy for the name of the person who said the quote
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+      
+      // Performing our AJAX GET request
+      $.ajax({
           url: queryURL,
           method: "GET"
         })
+        // After the data comes back from the API
+        .done(function(response) {
+          // Storing an array of results in the results variable
+          var results = response.data;
+
+          // Looping over every result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              // var gifDiv = $("<div class='item'>");
+
+              // Creating an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Prepending the results to the "#gifs-appear-here" div in the HTML
+              $("#gifs-appear-here").prepend(personImage);
+            }
+          }
+        });
+    });
+
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,68 +1,76 @@
- var shows = ["Game of Thrones", "Breaking Bad", "Mad Men", "The Sopranos", "The Wire"]
+// TV SHOW GIF LOCATER
+// DAVE BOKIL 2017
+// RUTGERS WEEK 7
 
 
-function renderButtons() {
-    $("#gifs-appear-here").empty();
-    for (var i = 0; i < shows.length; i++) {
-        var a = $("<button>");
-        a.addClass("button btn btn-default gifButton");
-        a.attr("data-person", shows[i]);
-        a.text(shows[i]);
-        $("#gifs-appear-here").append(a);
-console.log(a)
-    }
-}
-        console.log(shows)
+// jquery wrapper
+// $(document).ready(function() {
 
-        console.log(shows.length)
 
-renderButtons()
+  // Initial array of buttons
+  var shows = ["Game of Thrones", "Breaking Bad", "Mad Men", "The Sopranos", "Stranger Things"]
 
-// $("#addButton").on("click", function(event){
-//     event.preventDefault();
-//     var showrequest = $("#tvshowInput").val().trim();
-//     shows.push(showrequest);
-//     // renderButtons()
-// });
+  // Function to render buttons on to the page
+  function renderButtons() {
+      // empty the div first
+      $("#gifs-appear-here").empty();
+      // render buttons based on the length of the shows array
+      for (var i = 0; i < shows.length; i++) {
+          var a = $("<button>");
+          a.addClass("button btn btn-default gifButton");
+          a.attr("data-person", shows[i]);
+          a.text(shows[i]);
+          $("#tvButtons").append(a);
+      }
+  }
 
-    // Event listener for all button elements
-    $(".gifButton").on("click", function() {
+  renderButtons()
+
+  // Event listener for all button with gifButton class
+  $(".gifButton").on("click", function() {
       // In this case, the "this" keyword refers to the button that was clicked
-      var person = $(this).attr("data-person");
-      console.log(person)
+      var tvShow = $(this).attr("data-person");
+      console.log(tvShow)
 
       // Constructing a URL to search Giphy for the name of the person who said the quote
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
-      
-      // Performing our AJAX GET request
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      // Performing AJAX GET request
       $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        // After the data comes back from the API
-        .done(function(response) {
-          // Storing an array of results in the results variable
-          var results = response.data;
+              url: queryURL,
+              method: "GET"
+          })
+          // After the data comes back from the API
+          .done(function(response) {
+              // Storing an array of results in the results variable
+              var results = response.data;
 
-          // Looping over every result item
-          for (var i = 0; i < results.length; i++) {
+              // Looping over every result item
+              for (var i = 0; i < results.length; i++) {
 
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div with the class "item"
-              // var gifDiv = $("<div class='item'>");
 
-              // Creating an image tag
-              var personImage = $("<img>");
+                      // Creating an image tag
+                      var personImage = $("<img>");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              personImage.attr("src", results[i].images.fixed_height.url);
+                      // Giving the image tag an src attribute of a proprty pulled off the
+                      // result item
+                      personImage.attr("src", results[i].images.fixed_height.url);
 
-              // Prepending the results to the "#gifs-appear-here" div in the HTML
-              $("#gifs-appear-here").prepend(personImage);
-            }
-          }
-        });
-    });
+                      // Prepending the results to the "#gifs-appear-here" div in the HTML
+                      $("#gifs-appear-here").prepend(personImage);
+
+              }
+          });
+  });
+
+
+
+// }
+
+            
+
+
+
+
+
 
